9a8a1dbb2020280b428649c33621174938758de0
Pushpa kumari <pushpa.roy.2847@gmail.com>
2025-12-11 14:58:36 +0530

    feat: project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <p className="text-4xl">Hii Pushpa Kaisi ho? hello Anubhav shanki inshan</p>
+    </>
+  );
+}
+
+export default App;
